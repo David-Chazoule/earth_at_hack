78e176a8a90994f6992db901881611159af9222a
Alferrari31 <alferrari31@gmail.com>
2021-05-11 14:19:36 +0200

    first version

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header({ name }) {
+
+  return (
+    <div className="Header-container">
+      <h1>{name}</h1>
+      <h2>Les meilleurs produits de la galaxie</h2>
+    </div>
+  );
+}
+
+export default Header;
