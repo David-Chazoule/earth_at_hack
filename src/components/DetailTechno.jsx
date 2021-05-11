3c6341193cb3a18b220873776404eed878427622
Alferrari31 <alferrari31@gmail.com>
2021-05-11 21:43:38 +0200

    ShopperForm operational

--- src/components/DetailTechno.jsx
+++ src/components/DetailTechno.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ShopperForm from '../components/ShopperForm';
 
 import './DetailTechno.css';
 
+
 export default function DetailTechno(props) {
+
+const [showShopperForm, setShowShopperForm] = useState (false);
+
+const handleClick = () => {
+  setShowShopperForm(!showShopperForm);
+
+}
   return (
     <div className="DetailTechnoContainer">
       <h2>{props.name}</h2>
       <img src={props.image} alt="DetailTechnoImage" className="DetailTechnoImage"/>
       <p>{props.presentation}</p>
-      <button>I WANT IT FOR MY PLANET</button>  
+      <button onClick={handleClick}>I WANT IT FOR MY PLANET</button> 
+      {showShopperForm &&
+      <ShopperForm /> }
     </div>
   )
 }
