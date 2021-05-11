6f1b179563e8de200f03005fd894976243be5dbc
Paul-Emmanuel Legrand <paulm.legrand@gmail.com>
2021-05-12 00:15:55 +0200

    create functionals componnents

--- src/components/Sellers.jsx
+++ src/components/Sellers.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './Header';
+
 import './Sellers.css'
 
 export default function Sellers() {
   return (
-    <div>
+    <div className="Sellers-Main">
+      <Header />
       
     </div>
   )
---
+++ src/components/SellersCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SellersCard.css';
+
+
+export default function SellersCard() {
+  return (
+    <div className="SellersCardContainer">
+      
+    </div>
+  )
+}
