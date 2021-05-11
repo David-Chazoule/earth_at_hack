97f8a4deb9721b116189c3c9623a0b42aa24d0bb
David-Chazoule <d.chazoule@gmail.com>
2021-05-11 22:51:58 +0200

    style update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({ name }) {
   return (
     <div className="Header-container">
       <h1 className="Header-Title">{name}</h1>
-      <h2>Les meilleurs produits de la galaxie</h2>
+      <h2 className="slogan-title">Les meilleurs produits de la galaxie</h2>
     </div>
   );
 }
