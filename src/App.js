59cc5a2cf19231e0285f503e85a07383f8838172
Alferrari31 <alferrari31@gmail.com>
2021-05-11 17:25:23 +0200

    work on footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   const Appname = 'Mars°Ma°Zon';
   return (
     <div className="App">
        <Header name={Appname} />
+       <Footer />
     </div>
   );
 }
